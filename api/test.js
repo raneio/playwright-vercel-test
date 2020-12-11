3fbde8ed3235d60b6004816849dc58fb4da71bed
Rane <raneahonen@gmail.com>
2020-12-11 15:34:39 +0200

    Test chrome-aws-lambda

--- api/test.js
+++ api/test.js
@@ -1,11 +1,18 @@
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
 
 module.exports = async (req, res) => {
-  const browser = await puppeteer.launch();
+  const browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+
   const page = await browser.newPage();
   await page.goto('https://example.com');
+
   const pageTitle = await page.title();
-  
   await browser.close();
 
   res.json({
